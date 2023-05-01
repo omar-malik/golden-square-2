1a19145c90676016bc5a89b487f0526864ad41f0
omar <omar-malik22@hotmail.com>
2023-05-01 22:08:28 +0100

    wrote test for StringBuilder 'add' method

--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe StringBuilder do
   it "returns empty string" do
     string_builder = StringBuilder.new
     expect(string_builder.output).to eq ""
+    expect(string_builder.size).to eq 0
   end
-  
+
+  it "adds 5 letter word to string" do
+    string_builder = StringBuilder.new
+    string_builder.add("hello")
+    expect(string_builder.output).to eq "hello"
+    expect(string_builder.size).to eq 5
+  end
+
 end
